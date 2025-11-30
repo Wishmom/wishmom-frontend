aeb96fdf533c80a932351abc2d875c79472b933f
Wishmom <wishmom432@gmail.com>
2025-11-30 13:15:06 +0530

    1

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from './App.jsx';
 import { UserContextProvider } from './context/UserContext.jsx';
 
 
-// export const server = "https://wishmomserver.onrender.com"
+export const server = "https://wishmomserver.onrender.com"
 
-export const server = "http://localhost:5001"
+// export const server = "http://localhost:5001"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
